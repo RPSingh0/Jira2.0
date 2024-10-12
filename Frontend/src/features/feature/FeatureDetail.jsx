6e62f5d68bc55f74888d1594b278bcf1afd98e6f
RPSingh0 <rpalsingh715@gmail.com>
2024-10-12 22:42:18 +0530

    This commit -> * Added feature detail page * Added few icons to icon map

--- Frontend/src/features/feature/FeatureDetail.jsx
+++ Frontend/src/features/feature/FeatureDetail.jsx
@@ -1,15 +1,221 @@
-import {useParams} from "react-router-dom";
+import {Avatar, Box, Divider, InputAdornment, Paper, TextField, Typography} from "@mui/material";
+import TextEditor from "../../components/editor/Editor.jsx";
+import useDefaultEditor from "../../components/editor/useDefaultEditor.js";
+import {IconMap} from "../../utils/IconMap.jsx";
 
 function FeatureDetail() {
 
-    const {projectKey, featureKey} = useParams();
+    const createFeatureEditor = useDefaultEditor('Description for the feature goes here...')
 
     return (
-        <div>
-            Project Id: {projectKey}
-            <br/>
-            Feature Id: {featureKey}
-        </div>
+        <Box sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+            padding: "2rem"
+        }}>
+            <Typography variant={"h5"}>
+                Feature one (FTR-1)
+            </Typography>
+
+            <Box>
+                <Typography variant="body1" gutterBottom>
+                    Description
+                </Typography>
+                <TextEditor editor={createFeatureEditor} height={"20rem"}/>
+            </Box>
+            <Box>
+                <Typography variant="body1" gutterBottom>
+                    Stories
+                </Typography>
+                <TextField
+                    sx={(theme) => ({input: {color: theme.palette.defaultBlack.main}, marginBottom: "1rem"})}
+                    variant="outlined"
+                    size="small"
+                    placeholder="Search Stories"
+                    slotProps={{
+                        input: {
+                            endAdornment: <InputAdornment position="end">
+                                {IconMap['search']}
+                            </InputAdornment>,
+                        },
+                    }}
+                />
+                <Box sx={{
+                    display: "grid",
+                    gridTemplateRows: "1fr 1fr",
+                    rowGap: "1rem",
+                    columnGap: "1rem",
+                    overflowX: "auto",
+                    gridAutoFlow: "column",
+                    gridAutoColumns: "max-content",
+                    padding: "0.5rem 0.5rem 0.5rem 0"
+                }}>
+                    <StoryItemCard/>
+                    <StoryItemCard/>
+                    <StoryItemCard/>
+                    <StoryItemCard/>
+                    <StoryItemCard/>
+                    <StoryItemCard/>
+                    <StoryItemCard/>
+                    <StoryItemCard/>
+                    <StoryItemCard/>
+                    <StoryItemCard/>
+                    <StoryItemCard/>
+                    <StoryItemCard/>
+                </Box>
+            </Box>
+            <Divider/>
+            <Box>
+                <Typography variant="body1" gutterBottom>
+                    Bugs
+                </Typography>
+                <TextField
+                    sx={(theme) => ({input: {color: theme.palette.defaultBlack.main}, marginBottom: "1rem"})}
+                    variant="outlined"
+                    size="small"
+                    placeholder="Search Bugs"
+                    slotProps={{
+                        input: {
+                            endAdornment: <InputAdornment position="end">
+                                {IconMap['search']}
+                            </InputAdornment>,
+                        },
+                    }}
+                />
+                <Box sx={{
+                    display: "grid",
+                    gridTemplateRows: "1fr 1fr",
+                    rowGap: "1rem",
+                    columnGap: "1rem",
+                    overflowX: "auto",
+                    gridAutoFlow: "column",
+                    gridAutoColumns: "max-content",
+                    padding: "0.5rem 0.5rem 0.5rem 0"
+                }}>
+                    <BugItemCard/>
+                    <BugItemCard/>
+                    <BugItemCard/>
+                    <BugItemCard/>
+                    <BugItemCard/>
+                    <BugItemCard/>
+                    <BugItemCard/>
+                    <BugItemCard/>
+                    <BugItemCard/>
+                    <BugItemCard/>
+                    <BugItemCard/>
+                    <BugItemCard/>
+                </Box>
+            </Box>
+        </Box>
+    );
+}
+
+function StoryItemCard() {
+    return (
+        <Paper variant={"outlined"} elevation={2} sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "0.5rem",
+            width: "15rem",
+            padding: "0.5rem",
+            overflow: "hidden"
+        }}>
+            <Box sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                gap: "0.5rem"
+            }}>
+                {IconMap['userStory']}
+                <Typography variant={"overline"}>
+                    MFP2-1
+                </Typography>
+            </Box>
+            <Typography variant="caption" gutterBottom sx={{
+                display: "-webkit-box",
+                WebkitLineClamp: 2,
+                WebkitBoxOrient: "vertical",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                lineClamp: 2
+            }}>
+                Title for this story goes here and it can be very long also and it may go on for ever like this also
+            </Typography>
+            <Box sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between"
+            }}>
+                <Avatar>
+                    X
+                </Avatar>
+                <Box sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    gap: "0.2rem"
+                }}>
+                    <Typography variant="caption">Status</Typography>
+                    {IconMap['todo']}
+                </Box>
+            </Box>
+        </Paper>
+    );
+}
+
+function BugItemCard() {
+    return (
+        <Paper variant={"outlined"} elevation={2} sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "0.5rem",
+            width: "15rem",
+            padding: "0.5rem",
+            overflow: "hidden"
+        }}>
+            <Box sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                gap: "0.5rem"
+            }}>
+                {IconMap['bug']}
+                <Typography variant={"overline"}>
+                    MFP2-4
+                </Typography>
+            </Box>
+            <Typography variant="caption" gutterBottom sx={{
+                display: "-webkit-box",
+                WebkitLineClamp: 2,
+                WebkitBoxOrient: "vertical",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                lineClamp: 2
+            }}>
+                Title for this bug goes here and it can be very long also and it may go on for ever like this also
+            </Typography>
+            <Box sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between"
+            }}>
+                <Avatar>
+                    X
+                </Avatar>
+                <Box sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    gap: "0.2rem"
+                }}>
+                    <Typography variant="caption">Fix</Typography>
+                    {IconMap['done']}
+                </Box>
+            </Box>
+        </Paper>
     );
 }
 
--- Frontend/src/utils/IconMap.jsx
+++ Frontend/src/utils/IconMap.jsx
@@ -1,9 +1,23 @@
-import {AutoStories, BugReport, FeaturedVideo, Search, WorkHistory} from "@mui/icons-material";
+import {
+    AutoStories,
+    BugReport, CheckCircle,
+    FeaturedVideo,
+    KeyboardArrowUp,
+    KeyboardDoubleArrowUp, PlaylistAddCheck, PriorityHigh, RadioButtonChecked, RadioButtonUnchecked,
+    Search,
+    WorkHistory
+} from "@mui/icons-material";
 
 export const IconMap = {
     'work': <WorkHistory/>,
     'userStory': <AutoStories/>,
     'bug': <BugReport/>,
     'search': <Search/>,
-    'feature': <FeaturedVideo/>
+    'feature': <FeaturedVideo/>,
+    'low': <KeyboardArrowUp/>,
+    'medium': <KeyboardDoubleArrowUp/>,
+    'high': <PriorityHigh/>,
+    'todo': <PlaylistAddCheck/>,
+    'in-progress': <RadioButtonChecked/>,
+    'done': <CheckCircle/>
 }
